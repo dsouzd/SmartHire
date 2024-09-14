36da5cb8863495b1c839b275298a5aedd06304fd
dsouzd <117829012+dsouzd@users.noreply.github.com>
2024-09-14 19:06:25 +0530

    Style Changes for header

--- components/header.py
+++ components/header.py
@@ -1,45 +1,58 @@
-from dash import html
+import dash
+from dash import dcc, html
 import dash_bootstrap_components as dbc
 
-def header():
+def header(app):
     return dbc.Navbar(
         dbc.Container(
             [
-                # Logo and Title
+                # Logo on the left
                 dbc.Row(
                     [
                         dbc.Col(
-                            dbc.NavLink(
-                                html.Img(
-                                    src='/assets/img/logo_msg_global_RGB.svg',
-                                    height='50px'
-                                ),
-                                href='/',  # Link to the home page
+                            html.A(
+                                html.Img(src='/assets/img/logo_msg_global_RGB.svg', height="50px"),
+                                href="/",
                             ),
-                            width='auto'
+                            width="auto",
                         ),
                     ],
                     align="center",
-                    className="g-0",
+                    className="g-0",  # No spacing between columns
                 ),
-                # Navigation Links
+                
+                # Toggler for smaller screens
                 dbc.NavbarToggler(id="navbar-toggler"),
+                
+                # Navigation links
                 dbc.Collapse(
                     dbc.Nav(
                         [
-                            dbc.NavItem(dbc.NavLink("Home", href="/")),
-                            dbc.NavItem(dbc.NavLink("JD Preparation", href="/jdpreparation")),
-                            dbc.NavItem(dbc.NavLink("JD Screening", href="/screening")),
+                            dbc.NavLink("Home", href="/", className="nav-link"),
+                            dbc.NavLink("JD Preparation", href="/jdpreparation", className="nav-link"),
+                            dbc.NavLink("JD Screening", href="/screening", className="nav-link"),
+                            dbc.NavLink("Question Generation", href="/qfg", className="nav-link"),
                         ],
-                        className="ms-auto",
-                        navbar=True,
+                        className="ml-auto",  # Align links to the right
+                        navbar=True
                     ),
                     id="navbar-collapse",
-                    is_open=False,
                     navbar=True,
                 ),
             ]
         ),
-        color="dark",
-        dark=True,
+        color="white",  # Background color
+        dark=False,     # Text will not be white
+        className="header-navbar"
     )
+
+    # Callback to toggle navbar collapse for mobile view
+    @app.callback(
+        dash.dependencies.Output("navbar-collapse", "is_open"),
+        [dash.dependencies.Input("navbar-toggler", "n_clicks")],
+        [dash.dependencies.State("navbar-collapse", "is_open")],
+    )
+    def toggle_navbar(n_clicks, is_open):
+        if n_clicks:
+            return not is_open
+        return is_open
